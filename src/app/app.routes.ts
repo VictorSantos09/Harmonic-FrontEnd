a270b2e65d1d86376abd3485f063fead417edda7
Valdiclei <cacausoares474@gmail.com>
2024-04-18 18:50:59 -0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { PageRegisterComponent } from '../../projects/page-register/page-register.component';
 import { Routes } from '@angular/router';
 import { HomepageComponent } from '../../projects/homepage/homepage.component';
 import { PageCrudRadioComponent } from '../../projects/page-crud-radio/page-crud-radio.component';
@@ -5,4 +6,5 @@ import { PageCrudRadioComponent } from '../../projects/page-crud-radio/page-crud
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'radio', component: PageCrudRadioComponent },
+  { path: 'register', component: PageRegisterComponent },
 ];
