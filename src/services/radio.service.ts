12e0eb9114e7966044fdda513910d1561a3a9135
Victor Santos <110687512+VictorSantos09@users.noreply.github.com>
2024-05-16 21:10:51 -0300

    Update radio.service.ts

--- src/services/radio.service.ts
+++ src/services/radio.service.ts
@@ -12,16 +12,23 @@ export class RadioService {
   constructor(private _http: HttpClient) {}
 
   getAll(): Observable<ResponseData<RadioModel>> {
-    return this._http.get<ResponseData<RadioModel>>(`${API_URL.URL}Conteudo`);
+    return this._http.get<ResponseData<RadioModel>>(`${API_URL.URL}Conteudo`, {
+      withCredentials: true,
+    });
   }
 
   delete(id: number): Observable<ResponseData<RadioModel>> {
     return this._http.delete<ResponseData<any>>(
-      `${API_URL.URL}Conteudo/delete?id=${id}`
+      `${API_URL.URL}Conteudo/delete?id=${id}`,
+      {
+        withCredentials: true,
+      }
     );
   }
 
   insert(obj: any): Observable<Response> {
-    return this._http.post<Response>(`${API_URL.URL}Conteudo/add`, obj);
+    return this._http.post<Response>(`${API_URL.URL}Conteudo/add`, obj, {
+      withCredentials: true,
+    });
   }
 }
