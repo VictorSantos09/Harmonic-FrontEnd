3eb00bc8a6a9815349dbaedaa45f9af27aec4a45
Victor Santos <110687512+VictorSantos09@users.noreply.github.com>
2024-05-18 11:07:42 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_BACKEND_URL: 'https://harmonic-backend-bf50127b8e53.herokuapp.com',
+  API_BACKEND_URL: 'https://harmonic-backend-bf50127b8e53.herokuapp.com/',
 };
