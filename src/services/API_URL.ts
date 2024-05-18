5f4c51f57122f3f435f9659e07afe0cf1e65c9f9
Valdiclei <cacausoares474@gmail.com>
2024-05-17 21:45:27 -0300

    teste env var

--- src/services/API_URL.ts
+++ src/services/API_URL.ts
@@ -1,3 +1,10 @@
+import { environment } from '../environments/environment';
+
 export class API_URL {
   static readonly URL = 'https://localhost:7057/';
+
+  private _getURL() {
+    console.log(environment.production);
+    console.log(environment.API_BACKEND_URL);
+  }
 }
