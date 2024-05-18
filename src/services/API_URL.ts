1b13424a92e05f10e0ec0e06f979d369e9812e53
Victor Santos <110687512+VictorSantos09@users.noreply.github.com>
2024-05-18 11:04:07 -0300

    added proper file replacement

--- src/services/API_URL.ts
+++ src/services/API_URL.ts
@@ -1,11 +1,9 @@
 import { environment } from '../environments/environment';
 
 export class API_URL {
-  private static readonly URL_DEVELOPMENT = 'https://localhost:7057/';
   static readonly URL = this._getURL();
 
   private static _getURL() {
-    console.log(environment.production);
-    console.log(environment.API_BACKEND_URL);
+    return environment.API_BACKEND_URL;
   }
 }
