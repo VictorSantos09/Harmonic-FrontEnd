85ef95160ec8022e067d99c9c9661f4b641a8490
Valdiclei <cacausoares474@gmail.com>
2024-05-27 20:29:56 -0300

    moments criado

---
+++ projects/page-moments/page-moments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-moments',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-moments.component.html',
+  styleUrl: './page-moments.component.scss'
+})
+export class PageMomentsComponent {
+
+}
